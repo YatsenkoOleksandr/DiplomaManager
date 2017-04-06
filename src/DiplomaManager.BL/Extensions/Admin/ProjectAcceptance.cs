803f1cfda2bcedb57aa7f5323cf3bbaf04acf53e
YatsenkoOleksandr <yats.sasha@gmail.com>
2017-04-06 09:10:44 +0300

    Created interfaces IAdminService for getting projects/requests. Created enums and classes for sorting projects and paging

---
+++ src/DiplomaManager.BL/Extensions/Admin/ProjectAcceptance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaManager.BLL.Extensions.Admin
+{
+
+    /// <summary>
+    /// Enumeration for selecting requests
+    /// </summary>
+    public enum ProjectAcceptance
+    {
+        All,
+        Active,
+        Accepted,
+        Refused
+    }
+}
---
+++ src/DiplomaManager.BL/Services/AdminService.cs
@@ -0,0 +1,45 @@
+﻿using DiplomaManager.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiplomaManager.BLL.DTOs.ProjectDTOs;
+using DiplomaManager.BLL.Extensions;
+using DiplomaManager.BLL.Extensions.Admin;
+
+namespace DiplomaManager.BLL.Services
+{
+    public class AdminService : IAdminService
+    {
+        public IEnumerable<ProjectDTO> GetProjects(PageInfo pageInfo, ProjectFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<int, string> GetDegrees(ProjectFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<int, string> GetGroups(ProjectFilter filter)
+        {
+            throw new NotImplementedException();
+        } 
+
+        public IDictionary<int, string> GetStudents(ProjectFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<int, string> GetTeachers(ProjectFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<int, string> GetYears(ProjectFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
