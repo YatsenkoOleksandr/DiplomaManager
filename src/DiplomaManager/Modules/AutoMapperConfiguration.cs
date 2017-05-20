f17f721bb5eebf1f01dd933cec0230e02a25a2d2
YatsenkoOleksandr <yats.sasha@gmail.com>
2017-05-20 15:51:26 +0300

    Added ability to one-time configure automapper. Added class with mapping classes and added execution automapper configuration in Startup.cs

---
+++ src/DiplomaManager/Modules/AutoMapperConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiplomaManager.Modules
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(config =>
+            {
+                config.CreateMap<A, B>();
+            });
+        }
+    }
+}
