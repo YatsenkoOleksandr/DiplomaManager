ec1c1b79810c4236234c02e0ba3a75db446eb03d
TelAnd94 <teland94@mail.ru>
2017-05-22 11:47:42 +0300

    Fixed errors after merging

--- src/DiplomaManager.BL/Modules/ServiceModule.cs
+++ src/DiplomaManager.BL/Modules/ServiceModule.cs
@@ -1,6 +1,8 @@
 ﻿using Autofac;
 using DiplomaManager.BLL.Interfaces;
+using DiplomaManager.BLL.Interfaces.ProjectService;
 using DiplomaManager.BLL.Services;
+using DiplomaManager.BLL.Services.ProjectService;
 
 namespace DiplomaManager.BLL.Modules
 {
@@ -11,6 +13,8 @@ namespace DiplomaManager.BLL.Modules
             builder.RegisterType<StudentDiplomaRequestService>().As<IStudentDiplomaRequestService>();
             builder.RegisterType<UserService>().As<IUserService>();
             builder.RegisterType<TeacherDiplomaRequestService>().As<ITeacherDiplomaRequestService>();
+            builder.RegisterType<AdminService>().As<IAdminService>();
+            builder.RegisterType<AdminProjectService>().As<IAdminProjectService>();
             builder.RegisterType<ImportService>().As<IImportService>();
             builder.RegisterType<TranslationService>().As<ITranslationService>();
         }
