78a6cc476f7773ce38b32d275321f34a7e8d1b83
YatsenkoOleksandr <yats.sasha@gmail.com>
2017-05-23 01:13:07 +0300

    Implemented distribute algorithm without saving in database

---
+++ src/DiplomaManager.BL/Interfaces/IDistributionService.cs
@@ -0,0 +1,16 @@
+﻿using DiplomaManager.BLL.Extensions.DistributionService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaManager.BLL.Interfaces
+{
+    public interface IDistributionService
+    {
+        FormedProjects Distribute(int degreeId, int graduationYear);
+
+        void Save(FormedProjects formedProjects);
+    }
+}
