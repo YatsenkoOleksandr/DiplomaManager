de9653f8af99c88cd9ec16021ae3991cc7b5283f
TelAnd94 <teland94@mail.ru>
2017-05-24 01:59:59 +0300

    Add Distribution button

--- src/DiplomaManager/Areas/Admin/Controllers/DistributionController.cs
+++ src/DiplomaManager/Areas/Admin/Controllers/DistributionController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DiplomaManager.Areas.Admin.Controllers
@@ -14,6 +10,11 @@ namespace DiplomaManager.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Distribute()
+        {
+            return View("Index");
         }
     }
 }
