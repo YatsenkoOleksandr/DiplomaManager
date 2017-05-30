43615c1de769f55e654abfa5d887990891f1a9ec
YatsenkoOleksandr <yats.sasha@gmail.com>
2017-05-30 13:01:42 +0300

    Created entities for predefense system

---
+++ src/DiplomaManager.DAL/Entities/PredefenseEntities/Predefense.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DiplomaManager.DAL.Entities.StudentEntities;
+
+namespace DiplomaManager.DAL.Entities.PredefenseEntities
+{
+    public class Predefense
+    {
+        public int Id { get; set; }
+        
+        public int PredefenseDateId { get; set; }
+        public PredefenseDate PredefenseDate { get; set; }
+
+        public int StudentId { get; set; }
+        public Student Student { get; set; }        
+        
+        public bool? Passed { get; set; }  
+        
+        public int SoftwareReadiness { get; set; }
+
+        public int WritingReadiness { get; set; }
+        
+        public int PresentationReadiness { get; set; }
+        
+        public bool ReportExist { get; set; }
+        
+        public bool SafetySigned { get; set; }
+        
+        public bool EconomySigned { get; set; }
+        
+        public bool ControlSigned { get; set; }                  
+    }
+}
