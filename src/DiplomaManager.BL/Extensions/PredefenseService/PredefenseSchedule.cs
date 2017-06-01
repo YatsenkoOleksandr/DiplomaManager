f1ef67bcc3809c48b89331414ca6d79d288cfc61
YatsenkoOleksandr <yats.sasha@gmail.com>
2017-06-01 14:04:18 +0300

    Added new dto for describing predefense date. Fixed logic of getting predefense dates.

---
+++ src/DiplomaManager.BL/Extensions/PredefenseService/PredefenseSchedule.cs
@@ -0,0 +1,26 @@
+﻿using DiplomaManager.BLL.DTOs.PredefenseDTOs;
+using DiplomaManager.BLL.DTOs.TeacherDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaManager.BLL.Extensions.PredefenseService
+{
+    /// <summary>
+    /// Class discribes day of predefense
+    /// </summary>    
+    public class PredefenseSchedule
+    {
+        /// <summary>
+        /// Date with predefenses and students
+        /// </summary>
+        public PredefenseDateDTO PredefenseDate { get; set; }
+
+        /// <summary>
+        /// Teachers on predefense date
+        /// </summary>
+        public List<TeacherDTO> Teachers { get; set; }
+    }
+}
